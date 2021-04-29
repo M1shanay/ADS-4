06b72fd3cddc38acf9be2e0b1996b0697f5e0ccf
M1shanay <78785594+M1shanay@users.noreply.github.com>
2021-04-29 15:08:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,19 +2,15 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 #include <cassert>
-
-#include <iostream>
-#include <cassert>
 template<typename T>
-class TPQueue
-{
-private:
+class TPQueue {
+ private:
     T* arr;
     int size;
     int begin,
         end;
     int count;
-public:
+ public:
     TQueue(int = 100);
     ~TQueue();
 
@@ -24,21 +20,14 @@ public:
     bool isEmpty() const;
     bool isFull() const;
 };
-template<typename T>
-TQueue<T>::TQueue(int sizeQueue) :
-    size(sizeQueue),
-    begin(0), end(0), count(0)
-{
+template<typename T> TQueue<T>::TQueue(int sizeQueue): size(sizeQueue),
+begin(0), end(0), count(0) {
     arr = new T[size + 1];
 }
-template<typename T>
-TQueue<T>::~TQueue()
-{
+template<typename T> TQueue<T>::~TQueue() {
     delete[] arr;
 }
-template<typename T>
-void TQueue<T>::push(const T& item)
-{
+template<typename T> void TQueue<T>::push(const T& item) {
     assert(count < size);
     if (end != 0) {
         for (int i = end-1; i > -1; --i) {
@@ -53,49 +42,35 @@ void TQueue<T>::push(const T& item)
                 continue;
             }
         }
-     
     } else {
         arr[begin] = item;
-      
     }
     count++;
     end++;
 }
-template<typename T>
-T TQueue<T>::pop()
-{
-    
+template<typename T> T TQueue<T>::pop() {
     assert(count > 0);
-
     T item = arr[begin++];
     count--;
-
-   
     if (begin > size)
-        begin -= size + 1; 
-
+        begin -= size + 1;
     return item;
 }
-
 template<typename T>
-T TQueue<T>::get() const
-{
+T TQueue<T>::get() const {
     assert(count > 0);
     return arr[begin];
 }
-
 template<typename T>
-bool TQueue<T>::isEmpty() const
-{
+bool TQueue<T>::isEmpty() const {
     return count == 0;
 }
 template<typename T>
-bool TQueue<T>::isFull() const
-{
+bool TQueue<T>::isFull() const {
     return count == size;
 }
 struct SYM {
   char ch;
   int  prior;
 };
-#endif // INCLUDE_TPQUEUE_H_
+#endif
